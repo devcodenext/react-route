c7de7f47fa5629d63c89f5bd1bb1690643748288
devcodenext <devcodenext@outlook.com>
2024-02-03 14:45:53 +0530

    Added navigation feature and use of component from react-router-dom v6.21.3

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link, Navigate } from "react-router-dom";
 
 function Product() {
-  return <div>Product</div>;
+  const navigate = useNavigate();
+
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false;
+  });
+  return (
+    <div>
+      Product
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product
+      </button>
+      <Link to={`/product/details/1010`}>
+        <button>Navigate to Product Detail - 1010</button>
+      </Link>
+      {goToProduct && <Navigate to="/product/details/9780" />}
+      <button
+        onClick={() => {
+          setGoToProduct({ goToProduct: true });
+        }}
+      >
+        Navigate to Product - 9780 {useState}
+      </button>
+    </div>
+  );
 }
 
 export default Product;
